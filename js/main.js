17fd121f6a61e88ffd07e2e83f5f2d0b99a43cf8
Areli Jimenez <areli.jimenez.huerta@gmail.com>
2022-08-13 17:53:16 -0700

    Assignment06

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// HANDLE THE LOAD EVENT OF THE WINDOW
+window.addEventListener('load', () => {
+    console.log('Page is fully loaded');
+
+    // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
+    function openAddEmployee() {
+        window.open('add-employee.html', 'add-employee', 'width=600,height=600');
+    }
+
+    // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
+    let btn = window.document.getElementById('btnAddEmployee');
+    btn.addEventListener('click', openAddEmployee);
+
+})
